5b48a693c926c9f7cb35dd207a7c58897577756f
Brian <brian1419@gmail.com>
2023-02-22 17:51:51 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,10 +2,10 @@
 
 
 	/**
-	 * jack_bauer - prints every minute of the day of Jack Bauer
-	 * starting from 00:00 to 23:59
+	 * Displays the time in 24-hour format for every minute in a
+	 * day of Jack Bauer, from 00:00 to 23:59
 	 */
-	void jack_bauer(void)
+	void jack_bauer(void);
 	{
 		int i, j;
 
