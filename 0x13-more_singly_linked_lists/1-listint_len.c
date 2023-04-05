de9fc1cf0544cf1136a4b156052a3d297912430c
New Name <brian14198@gmail.com>
2023-04-04 17:27:55 -0700

    update

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in listint_t list.
+ * @h: Integer of listint_len
+ *
+ * Return: Number of elements in a linked list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+size_t num_node = 0;
+
+for (; h != NULL; h = h->next)
+	num_node++;
+
+return (num_node);
+}
