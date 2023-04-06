6bf1cc99b1856c381e85d44b384317bb3d3d9125
Brian-thee-shovi <123301449+Brian-thee-shovi@users.noreply.github.com>
2023-04-06 05:03:19 +0300

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,17 +9,17 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *many;
 
-	int num;
+	int lex;
 
 	if (!head || !*head)
 		return (0);
 
-	num = (*head)->n;
-	temp = (*head)->next;
+	lex = (*head)->n;
+	many = (*head)->next;
 	free(*head);
-	*head = temp;
+	*head = many;
 
-	return (num);
+	return (lex);
 }
