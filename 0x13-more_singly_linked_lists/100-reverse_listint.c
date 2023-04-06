6fc930553e11e2751a5d08ba8adb6aebfeb8f72c
Brian-thee-shovi <123301449+Brian-thee-shovi@users.noreply.github.com>
2023-04-06 04:35:03 +0300

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,17 +9,17 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *next = NULL;
+	listint_t *jue = NULL;
+	listint_t *lex = NULL;
 
 	while (*head)
 	{
-		next = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
-		*head = next;
+		lex = (*head)->lex;
+		(*head)->lex = jue;
+		jue = *head;
+		*head = lex;
 
 	}
-	*head = prev;
+	*head = jue;
 	return (*head);
 }
