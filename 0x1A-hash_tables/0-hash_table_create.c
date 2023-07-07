61c139abdf647e68a6d1a1cfe5761c4785edc038
Brian-thee-shovi <brian14198@gmail.com>
2023-07-07 06:32:10 -0700

    hash tables created innit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - this func creates a hash Table
+ * @size: array size
+ * Return: new hast table created
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hast_table_t *newtable_t;
+	unsigned long int ki;
+
+	newtable_t = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (newtable_t == NULL)
+		return (NULL);
+
+	newtable_t = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (newtable_t->array  == NULL)
+	{
+		free(newtable_t);
+		return (NULL);
+	}
+	for (ki = 0; ki < size; ki++)
+	{
+		newtable->array[ki] = NULL;
+	}
+	newtable_t->size = size;
+	return (newtable_t);
+}
+
