b4be88ce26264f772a49f1d7b11f7056f373e13a
Brian-thee-shovi <brian14198@gmail.com>
2023-10-11 21:18:00 +0300

    linear innit

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - this searches for a value using Ls i an array
+ * @array: pOinter to array to search
+ * @size: size of the array to search
+ * @value: value to search
+ * Return: 1st index where value is located and -1 if none
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t k;
+
+	if (array == NULL)
+		return (-1);
+
+	for (k = 0; k < size; k++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", k, array[k]);
+		        if (array[k] == value)
+				return (k);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,33 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @index: Index of the node in the list
+ * @next: pointer to the next node
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/*Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+int binary_recursion(int *array, size_t left, size_t right, int value);
+void print_array(int *array, size_t start, size_t end);
+
+#endif /*SEARCH_ALGOS_H*/
