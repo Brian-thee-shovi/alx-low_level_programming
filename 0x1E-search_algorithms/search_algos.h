f2d0e3ae9f0696e1547fbcc439441d4201903f15
Brian-thee-shovi <brian14198@gmail.com>
2023-10-12 01:11:00 -0700

    updates

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -31,6 +31,6 @@ int binary_recursion(int *array, size_t left, size_t right, int value);
 void print_array(int *array, size_t start, size_t end);
 int binary_search2(int *array, size_t left, size_t right, int value);
 int advanced_binary_recursive(int *array, size_t left, size_t right, int alue);
-listint_t *jump_list(listint_t *list, size_t size, int value)
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif /*SEARCH_ALGOS_H*/
